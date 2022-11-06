8443557b6e65866a438cece10a187a3e4c32ec06
Hudson <hudsonrodrigo06@gmail.com>
2022-11-06 20:44:11 -0300

    banco de dados utilizando EntityManager

--- delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
+++ delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.delivery.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +14,16 @@ import com.algaworks.delivery.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext   // notação para manager de comunicação model e banco de dados
+	private EntityManager manager; 
+	
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		var c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("João3");
-		c1.setTelefone("36529988");
-		c1.setEmail("jj@g.com");
+		return manager.createQuery("from Cliente", Cliente.class)
+			.getResultList();
 		
-		var c2 = new Cliente();
-		c2.setId(2L);
-		c2.setNome("Maria");
-		c2.setTelefone("36529977");
-		c2.setEmail("mm@g.com");
-		
-		return Arrays.asList(c1, c2);
 	}
 	
 }
--- delivery-api/src/main/java/com/algaworks/delivery/domain/model/Cliente.java
+++ delivery-api/src/main/java/com/algaworks/delivery/domain/model/Cliente.java
@@ -1,39 +1,44 @@
 package com.algaworks.delivery.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // --> apenas atributo com notação include será considerado no equal e hashcode
+
+@Entity
+//@Table(name = "tb_cliente") --> Exemplo de notação para nome diferente da classe
 public class Cliente {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name = "fone") // --> setar coluna de nome diferente do BD. 
 	private String telefone;
+	
+
 	
 	public Cliente() {
 		
 	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+	
 	
 	
 	
