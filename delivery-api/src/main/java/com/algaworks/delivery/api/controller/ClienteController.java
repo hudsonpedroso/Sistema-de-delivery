57c44ec5c7777af6a91b2f4fe8935eb7d87ccb08
Hudson <hudsonrodrigo06@gmail.com>
2022-11-18 15:27:12 -0300

    Cadastro de cliente utilizando metodo POST na API + JSON, com o devido @ResponseStatus relacionado

--- delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
+++ delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
@@ -7,10 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.delivery.domain.model.Cliente;
@@ -51,6 +55,13 @@ public class ClienteController {
 //		else {
 //			return ResponseEntity.notFound().build();
 //		}
+	}
+	
+	@PostMapping            //@RequestBody desserializa o body que vem em JSON para objeto Java
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente cadastrar(@RequestBody Cliente cliente) {
+		
+		return clienteRepository.save(cliente);
 	}
 	
 }
