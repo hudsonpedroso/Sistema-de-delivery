6e3d982939e4826941e232725c8647180be28a01
Hudson <hudsonrodrigo06@gmail.com>
2022-11-18 15:14:47 -0300

    adição da notação @RequestMapping para reduzir verbozidade e redundância de escrita de código nos caminhos da API

--- delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
+++ delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.delivery.domain.model.Cliente;
@@ -19,13 +20,14 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor // gera instância dos objetos declarados
 @RestController
+@RequestMapping("/clientes") //evita repetição de caminho para cada @GetMapping
 public class ClienteController {
 
 	
 	// @Autowired --> notação para criar automaticamente a instância do repository
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		
 		//return clienteRepository.findByNome("Joao da Silva");
@@ -33,7 +35,7 @@ public class ClienteController {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/obter/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> getCliente(@PathVariable Long clienteId) {
 
 		return clienteRepository.findById(clienteId)
