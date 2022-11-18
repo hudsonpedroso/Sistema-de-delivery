cb1f6e91529ce7d59ea06e303ac0d7c214923b5e
Hudson <hudsonrodrigo06@gmail.com>
2022-11-18 16:12:38 -0300

    Exclusão de clientes por id, via API e model Repository

--- delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
+++ delivery-api/src/main/java/com/algaworks/delivery/api/controller/ClienteController.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,4 +79,14 @@ public class ClienteController {
 		return ResponseEntity.ok(cliente);
 	}
 	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> excluir (@PathVariable Long clienteId) {
+		
+		if(!clienteRepository.existsById(clienteId))
+			return ResponseEntity.notFound().build();
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build(); //response no content() para responstas sem body
+	}
 }
